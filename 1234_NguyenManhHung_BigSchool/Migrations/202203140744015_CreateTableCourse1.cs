ccd06bd37038470f2d71c305e5451575ec126d1f
Mrhung189 <40222634+Mrhung189@users.noreply.github.com>
2022-03-14 15:41:57 +0700

    Add project files.

---
+++ 1234_NguyenManhHung_BigSchool/Migrations/202203140744015_CreateTableCourse1.cs
@@ -0,0 +1,16 @@
+namespace _1234_NguyenManhHung_BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableCourse1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
